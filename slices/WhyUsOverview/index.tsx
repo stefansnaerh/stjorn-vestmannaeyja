a0494521736b7520ee92fdb35b074de1759268c0
Stefán Snær <82455095+stefansnaerh@users.noreply.github.com>
2025-05-03 09:51:20 +0000

    push

--- slices/WhyUsOverview/index.tsx
+++ slices/WhyUsOverview/index.tsx
@@ -28,52 +28,66 @@ const WhyUsOverview = ({ slice }: WhyUsOverviewProps): JSX.Element => {
 
   return (
     <section
-      className="flex justify-center bg-backgroundLightBlue"
+      className="flex justify-center  self-center"
       data-slice-type={slice.slice_type}
       data-slice-variation={slice.variation}
     >
-      <Container className=" flex flex-col gap-fluid-56 w-fill">
-        <div className="flex flex-col gap-fluid-24">
-          <FadeIn className="w-fill">
-            <h2 className=" text-h2 font-headline text-headlineColor font-semiBold w-fill">
+      <div className=" flex flex-col gap-fluid-64 md:gap-fluid-96 w-fill max-w-[2000px]  ">
+        <div className="flex flex-col gap-fluid-24 bg-softBlue px-container-88 pt-64 lg-pt-fluid-122 pb-fluid-64  self-center">
+          <FadeIn className="w-fill flex flex-col gap-8">
+            <h2 className="text-[20px] font-headline text-buttonBlue font-semiBold w-fill">
+              {section.sub_title}
+            </h2>
+            <h2 className="text-h2 font-headline text-headlineColor font-semiBold w-fill">
               {section.title}
             </h2>
           </FadeIn>
-          <FadeIn className="w-[60%]">
-            <p className=" text-navLinks font-body font-regular text-bodyGrey">
-              {section.sub_title}
+          <FadeIn className=" md:w-[50%]">
+            <p className=" text-[16px] font-body font-regular text-bodyGrey">
+              {section.paragraph}
             </p>
           </FadeIn>
         </div>
-        <div className="flex flex-col lg:w-fill  lg:self-center sm:grid grid-cols-2 grid-rows-2 justify-center gap-fluid-32">
+        <div className="flex flex-col lg:w-fill  lg:self-center md:grid grid-cols-2 grid-rows-2 justify-center gap-fluid-40 md:gap-fluid-64 bg-backgroundGrey px-container-88 pb-fluid-64  ">
           {section.title_and_text_items.map((item, i) => {
             const iconToShow = (iconText: KeyTextField | void) => {
               if (iconText === "Launavernd") {
-                return <Launavernd className="h-fluid-40  self-start" />;
+                return (
+                  <Launavernd className="hidden sm:flex sm:w-[10%] md:w-[12%] md:self-start " />
+                );
               } else if (iconText === "Menntastyrkir") {
-                return <Menntastyrkur className="h-fluid-40  self-start" />;
+                return (
+                  <Menntastyrkur className="hidden sm:flex sm:w-[10%] md:w-[12%]   md:self-start " />
+                );
               } else if (iconText === "Heilsutengdir styrkir") {
-                return <Heilsustyrkir className="h-fluid-40  self-start" />;
-              } else return <Sjukrasjodur className="h-fluid-40  self-start" />;
+                return (
+                  <Heilsustyrkir className="hidden sm:flex sm:w-[10%] md:w-[12%] md:self-start" />
+                );
+              } else
+                return (
+                  <Sjukrasjodur className="hidden sm:flex sm:w-[10%] md:w-[12%]  md:self-start" />
+                );
             };
             return (
               <FadeIn
-                className="sm:w-fill sm:h-fill   self-center w-fill rounded-6 flex flex-col gap-fluid-24 bg-pureWhite  py-fluid-32 sm:py-fluid-40 px-fluid-32 xs:px-fluid-40 sm:px-fluid-56 border-[1px] border-greyInput border-opacity-30"
+                className="sm:w-fill sm:h-fill self-center w-fill rounded-6 flex sm:flex-row flex-col gap-fluid-40   border-[1px] border-greyInput border-opacity-20 sm:pb-fluid-40 sm:pt-fluid-48 px-fluid-32 xs:px-fluid-40 sm:px-fluid-56  bg-white  py-fluid-32"
                 key={`${i}${item.title}`}
               >
                 {iconToShow(item.title)}
-                <h4 className="text-h5  font-headline text-headlineColor font-medium ">
-                  {item.title}
-                </h4>
+                <div className="flex flex-col gap-fluid-24 sm:w-[85%]">
+                  <h4 className="text-h5  font-headline text-headlineColor font-medium ">
+                    {item.title}
+                  </h4>
 
-                <div className="w-fill  [&_a]:text-purple [&_a]:font-semiBold   [&_p]:text-[16px] [&_p]:font-regular [&_p]:font-body [&_p]:text-bodyGrey">
-                  <PrismicRichText field={item.paragraph} />
+                  <div className="w-fill  [&_a]:text-purple [&_a]:font-semiBold   [&_p]:text-[16px] [&_p]:font-regular [&_p]:font-body [&_p]:text-bodyGrey">
+                    <PrismicRichText field={item.paragraph} />
+                  </div>
                 </div>
               </FadeIn>
             );
           })}
         </div>
-      </Container>
+      </div>
     </section>
   );
 };
