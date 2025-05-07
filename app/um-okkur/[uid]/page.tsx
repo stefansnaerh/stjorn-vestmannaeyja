802197daf57f77ccf74a16a69eaa3f7bff8e3d03
Stefán Snær <82455095+stefansnaerh@users.noreply.github.com>
2025-05-07 09:28:31 +0000

    push

--- app/um-okkur/[uid]/page.tsx
+++ app/um-okkur/[uid]/page.tsx
@@ -34,29 +34,6 @@ export default async function Service({ params }: { params: Params }) {
   return (
     <section className="flex flex-col justify-center bg-backgroundGreyDarker">
       <div className="lg:pb-fluid-122 pt-fluid-40 smmd:pt-fluid-64  pb-64 px-container-56 xxs:px-container-88 self-center flex flex-col gap-fluid-56 smmd:w-[90%] lg2:w-[80%] max-w-[2000px] ">
-        <div className="flex flex-wrap gap-y-6 font-body text-sm  w-fit">
-          <Link
-            className="text-bodyGrey  hover:text-buttonBlue transition-all duration-300"
-            href={"/"}
-          >
-            Heim
-          </Link>
-          <Chevron className="h-fluid-16 rotate-90 self-center" />
-          <Link
-            className="text-sm text-bodyGrey hover:text-buttonBlue transition-all duration-300"
-            href={"/um-okkur"}
-          >
-            Um okkur
-          </Link>
-          <Image
-            alt="ör til hægri"
-            src={Chevron}
-            className="h-fluid-16 rotate-90 self-center"
-          />
-          <p className="text-sm text-headlineColor font-semiBold">
-            {section.title}
-          </p>
-        </div>
         <div className="mt-fluid-56 py-fluid-24 sm:py-fluid-48 px-fluid-24 sm:px-fluid-72  rounded-6 bg-pureWhite flex flex-col gap-fluid-32">
           <div>
             <h1 className="font-headline  text-headlineColor text-h2 font-semiBold">
